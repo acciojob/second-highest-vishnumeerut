d976b5cd51d28eb32f3169cebe481e5c7e5e8404
Vishnu Meerut <vermaji1891@gmail.com>
2025-01-28 15:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+			//Write your code here
+			arr.sort((a, b) => b-a);
+			console.log(arr[1]);
+			return arr[1]
+		}
+
+		function Main() {
+			var n = prompt("Enter the number of elements");
+			var arr = [];
+			for (var i = 0; i < n; i++) {
+				arr[i] = prompt("Enter element " + (i+1));
+			}
+			alert(secondHighest(arr));
+		}
+
+Main()
